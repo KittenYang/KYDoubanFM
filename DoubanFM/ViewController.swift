45f4206036d762a2c9cd00ac6aa6eb4b80b4ce98
KittenYang <kittenyang@icloud.com>
2015-04-09 17:54:06 +0800

    updated

--- DoubanFM/ViewController.swift
+++ DoubanFM/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+
         // Dispose of any resources that can be recreated.
     }
 
