d3684eddbd3702e9d3349225d50bc115c48e0cce
KittenYang <kittenyang@icloud.com>
2015-04-12 01:02:08 +0800

    updated

---
+++ DoubanFM/DoubanModel.swift
@@ -0,0 +1,13 @@
+//
+//  DoubanModel.swift
+//  DoubanFM
+//
+//  Created by Kitten Yang on 4/12/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+import UIKit
+
+class DoubanModel: NSObject {
+   
+}
